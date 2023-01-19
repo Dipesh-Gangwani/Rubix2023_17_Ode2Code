094134433ccc3a5ed4ed964df2e173b31fb7322f
Dipesh Gangwani <dagangwani101@gmail.com>
2023-01-19 15:59:41 +0530

    Added Nearbymarket and Guidelines Pages

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
-const navbtn = document.getElementById('nav-btn')
-navbtn.addEventListener('click' , button)
-function button(){
-    document.getElementById("nav").classList.toggle("active");
-}
+// const navbtn = document.getElementById('nav-btn')
+// navbtn.addEventListener('click' , button)
+// function button(){
+//   console.log('hii')
+//     document.getElementById("nav").classList.toggle("active");
+// }
 
 
 
 
 
-let slideIndex = 0;
-showSlides();
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
- let i;
-  let slides = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); 
-}
+// function showSlides() {
+//  let i;
+//   let slides = document.getElementsByClassName("slides");
+//   let dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 2000); 
+// }
